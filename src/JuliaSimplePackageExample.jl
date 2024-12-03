6bfae9ecab7b4425d7571d2f1834b0056aad5f31
mantzaris <avmantzaris@gmail.com>
2024-12-03 00:49:19 -0500

    putting in a function

--- src/JuliaSimplePackageExample.jl
+++ src/JuliaSimplePackageExample.jl
@@ -1,5 +1,16 @@
 module JuliaSimplePackageExample
 
-# Write your package code here.
+    export new_add
+
+    """
+    This function adds two numbers.
+
+    # Example(s)
+        julia> new_add(2,3)
+        5
+    """
+    function new_add(a, b)
+        return a + b
+    end
 
 end
